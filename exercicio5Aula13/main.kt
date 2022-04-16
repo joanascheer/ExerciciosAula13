1c6e9c403aaf4c3cfb89346225107ae6fadf8b19
Joana Scheer <44825683+joanascheer@users.noreply.github.com>
2022-04-16 12:55:25 -0300

    Update main.kt

--- exercicio5Aula13/main.kt
+++ exercicio5Aula13/main.kt
@@ -2,24 +2,7 @@ package exercicio5Aula13
 
 fun main() {
 
-    var quantidadeDeItens: Int = 0
-
-    println("Quantos itens deseja adicionar à sua lista? ")
-    quantidadeDeItens = readln().toInt()
-
-    var listaDeCompras = arrayOfNulls<String>(quantidadeDeItens)
-
-    for (i: Int in listaDeCompras.indices) {
-        println("Digite o item ${i + 1} de $quantidadeDeItens")
-        listaDeCompras[i] = readln()
-    }
-
-    println("***Lista de Compras *** \n")
-
-    for (i: Int in listaDeCompras.indices) {
-        println("Item ${i + 1}: listaDeCompras[i]")
-    }
-
+    montandoListaDeCompras()
 }
 
 
